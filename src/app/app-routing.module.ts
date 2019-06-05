d95bdac571168084f5da576ee2adeb05d7404a5a
chung <tranducchung1st@gmail.com>
2019-06-05 08:16:36 +0700

    Finish

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {BlogComponent} from './blog/blog.component';
 import {BolgEditComponent} from './bolg-edit/bolg-edit.component';
-import {BlogDetailComponent} from './blog-detail/blog-detail.component';
 
 const routes: Routes = [
   {
@@ -11,9 +10,6 @@ const routes: Routes = [
   }, {
     path: 'blog/:id/edit',
     component: BolgEditComponent
-  }, {
-    path: 'blog/:id',
-    component: BlogDetailComponent
   }
 ];
 
